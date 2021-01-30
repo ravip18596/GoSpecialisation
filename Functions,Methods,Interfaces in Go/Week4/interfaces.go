edeba97b16d099bdf8fa0828ce48a6e087fe8c32
ravip18596 <ravip18596@gmail.com>
2021-01-30 20:26:35 +0530

    finished week 4 assignment

--- Functions,Methods,Interfaces in Go/Week4/interfaces.go
+++ Functions,Methods,Interfaces in Go/Week4/interfaces.go
@@ -1,6 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cow struct {
+	name string
+}
+
+func (c cow) Eat() {
+	fmt.Println(fmt.Sprintf("%s eats grass",c.name))
+}
+
+func (c cow) Move() {
+	fmt.Println(fmt.Sprintf("%s walk",c.name))
+}
+
+func (c cow) Speak() {
+	fmt.Println(fmt.Sprintf("%s speaks moo",c.name))
+}
+
+type bird struct {
+	name string
+}
+
+func (b bird) Eat() {
+	fmt.Println(fmt.Sprintf("%s eats worms",b.name))
+}
+
+func (b bird) Move() {
+	fmt.Println(fmt.Sprintf("%s fly",b.name))
+}
+
+func (b bird) Speak() {
+	fmt.Println(fmt.Sprintf("%s speaks peep",b.name))
+}
+
+type snake struct {
+	name string
+}
+
+func (s snake) Eat() {
+	fmt.Println(fmt.Sprintf("%s eats mice",s.name))
+}
+
+func (s snake) Move() {
+	fmt.Println(fmt.Sprintf("%s moves like slither",s.name))
+}
+
+func (s snake) Speak() {
+	fmt.Println(fmt.Sprintf("%s speaks hsss",s.name))
+}
 
 type Animal interface {
 	Eat()
@@ -9,8 +62,62 @@ type Animal interface {
 }
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	objMap := make(map[string]Animal)
 	for {
-		fmt.Println("")
+		fmt.Println()
+		fmt.Println(fmt.Sprintf(`select between "%s" or "%s" command. Each line should contain 3 words starting with the command`, "newanimal", "query"))
+		fmt.Println("allowed animal types are ")
+		fmt.Println("cow")
+		fmt.Println("bird")
+		fmt.Println("snake")
 		fmt.Print("> ")
+		scanner.Scan()
+		command := scanner.Text()
+		commands := strings.Split(command, " ")
+		if len(commands) != 3 {
+			fmt.Println("Incorrect command. Please enter 3 string command")
+			break
+		}
+		switch strings.ToLower(commands[0]) {
+		case "newanimal":
+			var an Animal
+			animalName := commands[1]
+			objectType := commands[2]
+			switch strings.ToLower(objectType) {
+			case "cow":
+				an = cow{name: animalName}
+			case "bird":
+				an = bird{name: animalName}
+			case "snake":
+				an = snake{name: animalName}
+			default:
+				fmt.Println("error. incorrect object type. allowed types are cow, bird, snake.")
+			}
+			objMap[animalName] = an
+			fmt.Println("Created it!")
+		case "query":
+			animalName := commands[1]
+			action := commands[2]
+			var animal Animal
+			animal, ok := objMap[animalName]
+			if !ok {
+				fmt.Println(fmt.Sprintf("animal with name %s not present", animalName))
+				break
+			}
+			switch strings.ToLower(action) {
+			case "eat":
+				animal.Eat()
+			case "move":
+				animal.Move()
+			case "speak":
+				animal.Speak()
+			default:
+				fmt.Println("error. incorrect object type. allowed types are cow, bird, snake.")
+			}
+
+		default:
+			fmt.Println("error - incorrect first command. select b/w newanimal or query.")
+		}
 	}
 }
--- Functions,Methods,Interfaces in Go/Week4/problemStatement.md
+++ Functions,Methods,Interfaces in Go/Week4/problemStatement.md
@@ -1,4 +1,12 @@
-``` Write a program which allows the user to create a set of animals and to get information about those animals. Each animal has a name and can be either a cow, bird, or snake. With each command, the user can either create a new animal of one of the three types, or the user can request information about an animal that he/she has already created. Each animal has a unique name, defined by the user. Note that the user can define animals of a chosen type, but the types of animals are restricted to either cow, bird, or snake. The following table contains the three types of animals and their associated data.```
+```text
+Write a program which allows the user to create a set of animals and to get information about those animals.
+Each animal has a name and can be either a cow, bird, or snake. 
+With each command, the user can either create a new animal of one of the three types, or the user can request 
+information about an animal that he/she has already created.
+Each animal has a unique name, defined by the user. 
+Note that the user can define animals of a chosen type, but the types of animals are restricted to either cow, bird, or snake. 
+The following table contains the three types of animals and their associated data.
+```
 
 |Animal	| Food eaten | Locomtion method | Spoken sound |
 |-------|------------|------------------|--------------|
