2fc1a7e8cc6481337bc94db4516887fb1f27bf93
Ravi Prakash <ravip18596@gmail.com>
2024-11-19 18:36:44 +0530

    Changed Animal method to pointer receivers

--- Functions,Methods,Interfaces in Go/Week3/week3.go
+++ Functions,Methods,Interfaces in Go/Week3/week3.go
@@ -13,15 +13,15 @@ type Animal struct {
 	noise	   string
 }
 
-func (a Animal) Eat() {
+func (a *Animal) Eat() {
 	fmt.Println(a.food)
 }
 
-func (a Animal) Move() {
+func (a *Animal) Move() {
 	fmt.Println(a.locomotion)
 }
 
-func (a Animal) Speak() {
+func (a *Animal) Speak() {
 	fmt.Println(a.noise)
 }
 
